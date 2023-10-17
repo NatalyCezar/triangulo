035ae023882dce342c43b705001f32948fb5f2e5
gabisfreire <130668856+gabisfreire@users.noreply.github.com>
2023-10-17 09:29:35 -0300

    Commit Finish

--- triangulo.py
+++ triangulo.py
@@ -1,4 +1,4 @@
-#Comentário do tio.
+#Finalizado
 # crie um prog que leia os 3 lados de um triangulo
 # verificar se os lados de um polígono formam um triangulo
 # informar o tipo de triangulo formado
@@ -6,7 +6,14 @@ print('Para formar um Triangulo: ')
 ladoA = int(input('Digite o tamanho do lado A: '))
 ladoB = int(input('Digite o tamanho do lado B: '))
 ladoC = int(input('Digite o tamanho do lado C: '))
-if ladoA < (ladoB + ladoC) and ladoB < (ladoA + ladoC) and ladoC < (ladoA + ladoB):
-    print(f'Os lados formam um triangulo')
+#if ladoA < (ladoB + ladoC) and ladoB < (ladoA + ladoC) and ladoC < (ladoA + ladoB):
+    #print(f'Os lados formam um triangulo')
+#else:
+    #print(f'Os lados NÃO formam um triangulo')
+if (ladoA == ladoB) and (ladoA == ladoC):
+    print ('Equilatero')
+elif (ladoA == ladoB) or (ladoA == ladoC) or (ladoB == ladoC):
+    print ('Isósceles')
 else:
-    print(f'Os lados NÃO formam um triangulo')
+    print ('Escaleno')
+
